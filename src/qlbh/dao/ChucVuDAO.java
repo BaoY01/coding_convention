7c6d32e4d54bb4321c9117bc4c19688e01e1a9be
Nhu <lephucbaonhu2912@gmail.com>
2022-09-08 11:12:47 +0700

    sua ten bien & chu thich bang chucVu

--- src/qlbh/dao/ChucVuDAO.java
+++ src/qlbh/dao/ChucVuDAO.java
@@ -14,8 +14,10 @@ import qlbh.model.ChucVu;
  * @author Bao Nhu
  */
 public class ChucVuDAO {
+    
+    //lấy danh sách chức vụ
     public static ArrayList<ChucVu>layDanhSachChucVu(){
-        ArrayList<ChucVu> dscv = new ArrayList<ChucVu>();
+        ArrayList<ChucVu> danhSachChucVu = new ArrayList<ChucVu>();
        
         try {
             String sql = "Select * from ChucVu";
@@ -23,78 +25,81 @@ public class ChucVuDAO {
             provider.open();
             ResultSet resultSet = provider.excuteQuery(sql);
             while(resultSet.next()){
-                ChucVu cv = new ChucVu();
-                cv.setMaCV(resultSet.getString("MACV"));
-                cv.setTenCV(resultSet.getString("TENCV"));
-                cv.setLuong(resultSet.getFloat("LUONG"));
-                dscv.add(cv);
+                ChucVu chuVu = new ChucVu();
+                chuVu.setMaCV(resultSet.getString("MACV"));
+                chuVu.setTenCV(resultSet.getString("TENCV"));
+                chuVu.setLuong(resultSet.getFloat("LUONG"));
+                danhSachChucVu.add(chuVu);
             }
             provider.Close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return dscv;
+        return danhSachChucVu;
     }
     
-    
-    public static boolean themChucVu(ChucVu cv){
-        boolean kq = false;
-        String sql = String.format("insert into chucvu(macv, tencv, luong) values('%s', N'%s', '%f');", cv.getMaCV(), cv.getTenCV(), cv.getLuong());
+    //thêm chức vụ
+    public static boolean themChucVu(ChucVu chucVu){
+        boolean ketQua = false;
+        String sql = String.format("insert into chucvu(macv, tencv, luong) values('%s', N'%s', '%f');", chucVu.getMaCV(), chucVu.getTenCV(), chucVu.getLuong());
         SQLServerDataProvider provider = new SQLServerDataProvider();
         provider.open();
         int n = provider.excuteUpdate(sql);
         if(n == 1){
-            kq = true;
+            ketQua = true;
         }
         provider.Close();
-        return kq;
+        return ketQua;
     }
  
-    public static boolean xoaChucVu(String macv){
-        boolean kq = false;
-        String sql = String.format("delete from chucvu where macv='%s'", macv);
+    //xóa chức vụ
+    public static boolean xoaChucVu(String maChucVu){
+        boolean ketQua= false;
+        String sql = String.format("delete from chucvu where macv='%s'", maChucVu);
         SQLServerDataProvider provider = new SQLServerDataProvider();
         provider.open();
         int n = provider.excuteUpdate(sql);
         if(n == 1){
-            kq = true;
+            ketQua = true;
         }
         provider.Close();
-        return kq;
+        return ketQua;
     }
     
-    public static ArrayList<ChucVu>timKiemChucVuTheoMa(String macv){
-        ArrayList<ChucVu> dscv = new ArrayList<ChucVu>();
+    //tìm kiếm chức vụ
+    public static ArrayList<ChucVu>timKiemChucVuTheoMa(String maChuVu){
+        ArrayList<ChucVu> danhSachChuVu = new ArrayList<ChucVu>();
         try {
-            String sql = "Select * from chucvu where macv like '%" + macv + " %'";
+            String sql = "Select * from chucvu where macv like '%" + maChuVu + " %'";
             SQLServerDataProvider provider = new SQLServerDataProvider();
             provider.open();
             ResultSet resultSet = provider.excuteQuery(sql);
             while(resultSet.next()){
-                ChucVu cv = new ChucVu();
-                cv.setMaCV(resultSet.getString("macv"));
-                cv.setTenCV(resultSet.getString("tencv"));
-                cv.setLuong(resultSet.getFloat("luong"));
-                dscv.add(cv);
+                ChucVu chucVu = new ChucVu();
+                chucVu.setMaCV(resultSet.getString("macv"));
+                chucVu.setTenCV(resultSet.getString("tencv"));
+                chucVu.setLuong(resultSet.getFloat("luong"));
+                danhSachChuVu.add(chucVu);
             }
             provider.Close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return dscv;
+        return danhSachChuVu;
     }
     
-    public static boolean capNhatChucVu(ChucVu cv){
-        boolean kq = false;
-        String sql = String.format("update chucVu set tencv = N'%s', luong = '%f' " + "where macv= '%s'", cv.getTenCV(), cv.getLuong(), cv.getMaCV());
+    //cập nhật chức vụ
+    public static boolean capNhatChucVu(ChucVu chucVu){
+        boolean ketQua = false;
+        String sql = String.format("update chucVu set tencv = N'%s', luong = '%f' " + "where macv= '%s'", chucVu.getTenCV(), chucVu.getLuong(), chucVu.getMaCV());
         SQLServerDataProvider provider = new SQLServerDataProvider();
         provider.open();
         int n = provider.excuteUpdate(sql);
         if(n == 1){
-            kq = true;
+            ketQua = true;
         }
         provider.Close();
-        return kq;
+        return ketQua;
     }
     
 }
